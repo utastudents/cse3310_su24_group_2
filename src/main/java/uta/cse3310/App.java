6d2d3c452e78f14ae680c3af0dfa29cea071ee66
chriscod3 <caa7232@mavs.uta.edu>
2024-07-11 17:35:15 -0500

    Added the word.txt file to a resource folder

---
+++ src/main/java/uta/cse3310/App.java
@@ -0,0 +1,13 @@
+package uta.cse3310;
+
+public class App {
+    public static void main(String[] args) {
+        int httpPort = Integer.parseInt(System.getenv().getOrDefault("HTTP_PORT", "9180"));
+        int websocketPort = httpPort + 100;
+
+        Game game = new Game();
+        WebSocket webSocketServer = new WebSocket(websocketPort, game);
+
+
+    }
+}
--- src/main/java/uta/cse3310/WebSocket.java
+++ src/main/java/uta/cse3310/WebSocket.java
@@ -1,80 +1,15 @@
 package uta.cse3310;
 
-import java.net.InetSocketAddress;
-import java.nio.ByteBuffer;
-
-import org.java_websocket.WebSocket;
-import org.java_websocket.handshake.ClientHandshake;
-import org.java_websocket.server.WebSocketServer;
-
-import com.google.gson.Gson;
-
-public class WebSocket extends WebSocketServer {
-
-    public WebSocket(int port) {
-        super(new InetSocketAddress(port));
-    }
-
-    @Override
-    public void onOpen(WebSocket conn, ClientHandshake handshake) {
-        connectionId++;
-        System.out.println(conn.getRemoteSocketAddress().getAddress().getHostAddress() + " connected");
-
-        ServerEvent E = new ServerEvent();
-        Game G = new Game();
-        conn.setAttachment(G);
-
-        Gson gson = new Gson();
-        String jsonString = gson.toJson(E);
-        conn.send(jsonString);
-    }
-
-    @Override
-    public void onClose(WebSocket conn, int code, String reason, boolean remote) {
-        System.out.println(conn + " has closed");
-        Game G = conn.getAttachment();
-        G = null;
-    }
-
-    @Override
-    public void onMessage(WebSocket conn, String message) {
-        System.out.println("Received message: " + message);
-
-        Gson gson = new Gson();
-        UserEvent U = gson.fromJson(message, UserEvent.class);
-
-        Game G = conn.getAttachment();
-        G.Update(U);
-
-        String jsonString = gson.toJson(G);
-        broadcast(jsonString);
-    }
-
-    @Override
-    public void onMessage(WebSocket conn, ByteBuffer message) {
-        System.out.println(conn + ": " + message);
-    }
-
-    @Override
-    public void onError(WebSocket conn, Exception ex) {
-        ex.printStackTrace();
-        if (conn != null) {
-            
-        }
-    }
-
-    @Override
-    public void onStart() {
-        setConnectionLostTimeout(0);
-
-    }
-
+public class App {
     public static void main(String[] args) {
-        int port = 9180; 
-        WebSocket server = new WebSocket(port);
-        server.setReuseAddr(true);
-        server.start();
-        System.out.println("WebSocket server started on port: " + port);
+        int httpPort = Integer.parseInt(System.getenv().getOrDefault("HTTP_PORT", "9180"));
+        int websocketPort = httpPort + 100;
+
+        Game game = new Game();
+        WebSocket webSocketServer = new WebSocket(websocketPort, game);
+
+
     }
 }
+
 
