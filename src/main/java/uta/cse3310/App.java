43df2366557de580606fd2e1098c799625f8da6a
Rakshya Basnet <rakshyabasnet@Rakshyas-MBP.dhost.uta.edu>
2024-07-24 14:17:38 -0500

    Update App.java with the addition of the class HttpServerHandler

--- src/main/java/uta/cse3310/App.java
+++ src/main/java/uta/cse3310/App.java
@@ -1,14 +1,7 @@
 package uta.cse3310;
 
-import com.sun.net.httpserver.HttpServer;
-import com.sun.net.httpserver.HttpHandler;
-import com.sun.net.httpserver.HttpExchange;
 import org.java_websocket.server.WebSocketServer;
-
 import java.io.IOException;
-import java.net.InetSocketAddress;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.Vector;
 
 public class App {
@@ -18,33 +11,19 @@ public class App {
         // Read the HTTP port 
         String httpPortStr = System.getenv("HTTP_PORT");
         int httpPort = (httpPortStr != null) ? Integer.parseInt(httpPortStr) : 9080;
-
-
         int websocketPort = httpPort + 100;
 
-        // This starts the HTTP server
-    /*    try {
-            HttpServer httpServer = HttpServer.create(new InetSocketAddress(httpPort), 0);
-            httpServer.createContext("/", new HttpHandler() {
-                @Override
-                public void handle(HttpExchange exchange) throws IOException {
-                    String filePath = "src/main/resources/index.html";
-                    byte[] response = Files.readAllBytes(Paths.get(filePath));
-                    exchange.sendResponseHeaders(200, response.length);
-                    exchange.getResponseBody().write(response);
-                    exchange.getResponseBody().close();
-                }
-            });
-            httpServer.start();
-            System.out.println("HTTP server started on port " + httpPort);
+        // Start the HTTP server
+        HttpServerHandler httpServerHandler = new HttpServerHandler(httpPort);
+        try {
+            httpServerHandler.start();
         } catch (IOException e) {
             e.printStackTrace();
-        }*/
+        }
 
-        // starts the WebSocket server
+        // Start the WebSocket server
         WebSocketHandler webSocketServer = new WebSocketHandler(websocketPort);
         webSocketServer.start();
         System.out.println("WebSocket server started on port " + websocketPort);
     }
 }
-
