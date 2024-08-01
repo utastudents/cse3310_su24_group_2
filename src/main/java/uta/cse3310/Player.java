00da6451dc0edf7e31364326bdd08296308ece71
rakshyab02 <rxb2826@mavs.uta.edu>
2024-07-31 22:52:21 -0500

    Updated Player.java

--- src/main/java/uta/cse3310/Player.java
+++ src/main/java/uta/cse3310/Player.java
@@ -1,40 +1,39 @@
 //Player.java reps a player; their score and status online or not
 package uta.cse3310;
-
 public class Player {
-    private String playerId;
-    private String name;
-    private int score;
-    private boolean isOnline;
+    private String playerId;
+    private String name;
+    private int score;
+    private boolean isOnline;
 
-    public Player(String playerId, String name) {
-        this.playerId = playerId;
-        this.name (http://this.name/) = name;
-        this.score = 0;
-        this.isOnline = true;
-    }
+    public Player(String playerId, String name) {
+        this.playerId = playerId;
+        this.name = name;
+        this.score = 0;
+        this.isOnline = true;
+    }
 
-    public void updateScore(int points) {
-        this.score += points;
-    }
+    public void updateScore(int points) {
+        this.score += points;
+    }
 
-    public void setOnlineStatus(boolean status) {
-        this.isOnline = status;
-    }
+    public void setOnlineStatus(boolean status) {
+        this.isOnline = status;
+    }
 
-    public int getScore() {
-        return score;
-    }
+    public int getScore() {
+        return score;
+    }
 
-    public String getPlayerId() {
-        return playerId;
-    }
+    public String getPlayerId() {
+        return playerId;
+    }
 
-    public String getName() {
-        return name;
-    }
+    public String getName() {
+        return name;
+    }
 
-    public boolean isOnline() {
-        return isOnline;
-    }
+    public boolean isOnline() {
+        return isOnline;
+    }
 }
