e49975006fb8ea36fb1354848d78b11e854f6530
rakshyab02 <rxb2826@mavs.uta.edu>
2024-07-31 22:54:50 -0500

    Updated Scoreboard.java

--- src/main/java/uta/cse3310/Scoreboard.java
+++ src/main/java/uta/cse3310/Scoreboard.java
@@ -6,21 +6,21 @@ import java.util.Comparator;
 import java.util.List;
 
 public class Scoreboard {
-    private List<Player> highScores;
+    private List<Player> highScores;
 
-    public Scoreboard() {
-        highScores = new ArrayList<>();
-    }
+    public Scoreboard() {
+        highScores = new ArrayList<>();
+    }
 
-    public void updateScoreboard(Player player) {
-        highScores.add(player);
-        highScores.sort(Comparator.comparingInt(Player::getScore).reversed());
-        if (highScores.size() > 10) {
-            highScores = highScores.subList(0, 10);
-        }
-    }
+    public void updateScoreboard(Player player) {
+        highScores.add(player);
+        highScores.sort(Comparator.comparingInt(Player::getScore).reversed());
+        if (highScores.size() > 10) {
+            highScores = highScores.subList(0, 10);
+        }
+    }
 
-    public List<Player> getTopPlayers() {
-        return new ArrayList<>(highScores);
-    }
+    public List<Player> getTopPlayers() {
+        return new ArrayList<>(highScores);
+    }
 }
