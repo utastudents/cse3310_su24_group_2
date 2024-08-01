892b5b0e47008b56c24d68c8507576694470478a
rakshyab02 <rxb2826@mavs.uta.edu>
2024-07-31 23:16:47 -0500

    Unit test for GameServer.java

--- src/test/java/uta/cse3310/GameServerTest.java
+++ src/test/java/uta/cse3310/GameServerTest.java
@@ -4,58 +4,95 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.List;
 
-public class GameServerTest {
-    private GameServer gameServer;
+class GameServerTest {
 
-    @BeforeEach
-    public void setUp() {
-        gameServer = new GameServer(8080, 9000);
-    }
+private GameServer gameServer;
 
-    @Test
-    public void testStart() {
-        gameServer.start();
-       
-    }
+@BeforeEach
+void setUp() {
+gameServer = new GameServer(8080, 8081);
+}
 
-    @Test
-    public void testFindOrCreateGameSession() {
-        GameSession session = gameServer.findOrCreateGameSession();
-        assertNotNull(session);
-        assertTrue(gameServer.getGameSessions().contains(session));
-    }
+@Test
+void testCreateAndJoinLobby() {
+String lobbyId = "testLobby";
+gameServer.createLobby(lobbyId);
 
-    @Test
-    public void testRemoveGameSession() {
-        GameSession session = gameServer.findOrCreateGameSession();
-        gameServer.removeGameSession(session);
-        assertFalse(gameServer.getGameSessions().contains(session));
-    }
+Player player1 = new Player("player1", "Player One");
+GameSession session = gameServer.joinLobby(lobbyId, player1);
 
-    @Test
-    public void testUpdateScoreboard() {
-        Player player = new Player("1", "Rakshya");
-        gameServer.updateScoreboard(player);
-        assertTrue(gameServer.getTopPlayers().contains(player));
-    }
+assertNotNull(session);
+assertEquals(1, session.getPlayers().size());
+assertEquals("Player One", session.getPlayers().get(0).getName());
+}
 
-    @Test
-    public void testRemovePlayerFromSession() {
-        GameSession session = gameServer.findOrCreateGameSession();
-        Player player = new Player("1", "Rakshya");
-        session.addPlayer(player);
-        gameServer.removePlayerFromSession(player.getPlayerId());
-        assertFalse(session.getPlayers().contains(player));
-    }
+@Test
+void testRemovePlayerFromSession() {
+String lobbyId = "testLobby";
+gameServer.createLobby(lobbyId);
 
-    @Test
-    public void testGetPlayerCount() {
-        GameSession session = gameServer.findOrCreateGameSession();
-        Player player1 = new Player("1", "Rakshya");
-        Player player2 = new Player("2", "Chris");
-        session.addPlayer(player1);
-        session.addPlayer(player2);
-        assertEquals(2, gameServer.getPlayerCount());
-    }
+Player player1 = new Player("player1", "Player One");
+gameServer.joinLobby(lobbyId, player1);
+
+gameServer.removePlayerFromSession("player1");
+
+GameSession session = gameServer.findSessionForPlayer("player1");
+assertNull(session);
+}
+
+@Test
+void testGetAvailableLobbies() {
+gameServer.createLobby("lobby1");
+gameServer.createLobby("lobby2");
+
+List<String> lobbies = gameServer.getAvailableLobbies();
+
+assertEquals(2, lobbies.size());
+assertTrue(lobbies.contains("lobby1"));
+assertTrue(lobbies.contains("lobby2"));
+}
+
+@Test
+void testFindSessionForPlayer() {
+String lobbyId = "testLobby";
+gameServer.createLobby(lobbyId);
+
+Player player1 = new Player("player1", "Player One");
+gameServer.joinLobby(lobbyId, player1);
+
+GameSession session = gameServer.findSessionForPlayer("player1");
+
+assertNotNull(session);
+assertEquals(lobbyId, session.getLobbyId());
+}
+
+@Test
+void testUpdateScoreboard() {
+Player player1 = new Player("player1", "Player One");
+player1.updateScore(100);
+
+gameServer.updateScoreboard(player1);
+
+List<Player> topPlayers = gameServer.getTopPlayers();
+
+assertEquals(1, topPlayers.size());
+assertEquals("Player One", topPlayers.get(0).getName());
+assertEquals(100, topPlayers.get(0).getScore());
+}
+
+@Test
+void testGetPlayerCount() {
+String lobbyId1 = "lobby1";
+String lobbyId2 = "lobby2";
+gameServer.createLobby(lobbyId1);
+gameServer.createLobby(lobbyId2);
+
+gameServer.joinLobby(lobbyId1, new Player("player1", "Player One"));
+gameServer.joinLobby(lobbyId1, new Player("player2", "Player Two"));
+gameServer.joinLobby(lobbyId2, new Player("player3", "Player Three"));
+
+assertEquals(3, gameServer.getPlayerCount());
+}
 }
